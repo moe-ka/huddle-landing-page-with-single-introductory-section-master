3c10616b4297bff755fb8bc3dbe7c904422fe036
moe-ka <rizkiizzulhaq14@gmail.com>
2022-10-16 17:19:01 +0700

    update file

--- js/script.js
+++ js/script.js
@@ -11,4 +11,30 @@ const mp = document.createElement('img');
 mp.setAttribute('src', 'images/illustration-mockups.svg');
 mockup.appendChild(mp);
 
+const tombol = document.getElementById('register');
+tombol.addEventListener('click', function(e){
+    if(document.body.className == 'black-mode'){
+        document.body.classList.remove('black-mode');
+        document.body.classList.add('normal');   
+    }else{
+        document.body.classList.remove('normal');   
+        document.body.classList.add('black-mode');
+    }
+});
 
+const type = document.querySelector('.title span');
+
+
+let i = 0;
+var txt = 'Your Fans Will Love';
+var speed = 200;
+
+function typeWritter(){
+    if(i < txt.length){
+        type.innerHTML += txt.charAt(i);
+        i++
+        setTimeout(typeWritter, speed);
+    }
+}
+
+typeWritter();
